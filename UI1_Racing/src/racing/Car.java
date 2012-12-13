cb6f503b2528d3ea5d6dd2cb1e762c8446b3040d
pkaufman <pkaufman@hsr.ch>
2012-12-13 16:38:28 +0100

    start

---
+++ UI1_Racing/src/racing/Car.java
@@ -0,0 +1,159 @@
+package racing;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class Car extends JComponent {
+
+	private static final long serialVersionUID = -8147987130820080891L;
+	public static final double SLOW_SPEED = 1;
+	public static final double MAX_FORWARD_SPEED = 6;
+	public static final double MAX_BACKWARD_SPEED = -2;
+	private double x = 0, y = 0;
+	private float angle = 0;
+	private double speed = 0.0;
+
+	public Car() {
+	}
+
+	@Override
+	public void move(final int x, final int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void move(final double x, final double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public void move() {
+		final double newX = x + Math.sin(angle) * speed;
+		final double newY = y - Math.cos(angle) * speed;
+		move(newX, newY);
+	}
+
+	@Override
+	protected void paintComponent(final Graphics g) {
+		super.paintComponent(g);
+		final Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		/*
+		 * Verschieben des Zeichenrasters um x und y drehen der Fl�che um den
+		 * Winkle angle
+		 */
+		g2d.translate(x, y);
+		g2d.rotate(angle);
+
+		// Beispielzeichnung ersetzten durch ihre eigene
+		g2d.setPaint(Color.red);
+		g2d.fillRect(-15, -20, 30, 40);
+		g2d.setPaint(new Color(0x727272));
+		g2d.fillRect(-20, -18, 5, 10);
+		g2d.fillRect(15, -18, 5, 10);
+		g2d.fillRect(-20, 8, 5, 10);
+		g2d.fillRect(15, 8, 5, 10);
+
+		g2d.setPaint(Color.yellow);
+		g2d.fillRect(-12, 10, 24, 5);
+
+		g2d.setPaint(new Color(0xBFD7FF));
+		g2d.fillPolygon(new int[] { 0, -10, 10 }, new int[] { -17, 5, 5 }, 3);
+
+	}
+
+	private int doubleToInt(final double doubleValue) {
+		final int baseInt = (int) doubleValue;
+		if (doubleValue - baseInt >= 0.5) {
+			return baseInt + 1;
+		} else {
+			return baseInt;
+		}
+	}
+
+	public void accelerate() {
+		if (speed <= MAX_FORWARD_SPEED) {
+			speed += 0.1;
+		}
+	}
+
+	public void slowdown() {
+		slowdown(1);
+	}
+
+	public void slowdown(final double factor) {
+		speed -= 0.2 * factor;
+		if (speed < 0) {
+			speed = 0;
+		}
+	}
+
+	public void setSlowSpeed() {
+		if (speed != SLOW_SPEED) {
+			slowdown(2.8);
+		}
+	}
+
+	public void stop() {
+		speed = 0;
+	}
+
+	public void turnRight() {
+		angle += Math.PI / 40;
+	}
+
+	public void turnLeft() {
+		angle -= Math.PI / 40;
+	}
+
+	@Override
+	public int getX() {
+		return doubleToInt(x);
+	}
+
+	@Override
+	public int getY() {
+		return doubleToInt(y);
+	}
+
+	public float getAngle() {
+		return angle;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setAngle(final float angle) {
+		this.angle = angle;
+	}
+
+	public static void main(final String[] args) {
+		final JFrame f = new JFrame("Racing");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		final Car c = new Car();
+		c.move(50, 50);
+		f.add(c);
+		f.setSize(200, 240);
+		f.setVisible(true);
+
+	}
+
+	public void deccelerate() {
+		if (speed >= MAX_BACKWARD_SPEED) {
+			speed -= 0.1;
+		}
+	}
+
+	public void setMinSlowSpeed() {
+		speed = -SLOW_SPEED;
+
+	}
+
+}
---
+++ UI1_Racing/src/racing/Racing.java
@@ -0,0 +1,204 @@
+package racing;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+
+public class Racing extends JPanel implements ActionListener {
+
+	private static final long serialVersionUID = 1169331112688629681L;
+	private Car raceCar;
+	private int[] keys;
+	private Dimension screenSize;
+	private Timer timer;
+	private BufferedImage track;
+	private BufferedImage texture;
+
+	public Racing() {
+		init();
+	}
+
+	private void init() {
+		try {
+			track = ImageIO.read(new File("data/Racing01.png"));
+			texture = ImageIO.read(new File("data/Grass.jpg"));
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+
+		screenSize = new Dimension(600, 600);
+		setPreferredSize(screenSize);
+
+		raceCar = new Car();
+		raceCar.move(300, 300);
+
+		registerKeyListener();
+
+		timer = new Timer(15, this);
+		timer.start();
+	}
+
+	private void registerKeyListener() {
+		keys = new int[256];
+		Arrays.fill(keys, 0); // 0 = key is up
+
+		final KeyboardFocusManager kfm = KeyboardFocusManager
+				.getCurrentKeyboardFocusManager();
+		kfm.addKeyEventDispatcher(new KeyEventDispatcher() {
+			@Override
+			public boolean dispatchKeyEvent(final KeyEvent e) {
+				switch (e.getID()) {
+				case KeyEvent.KEY_PRESSED:
+					keys[e.getKeyCode()] = 1;
+					break;
+				case KeyEvent.KEY_RELEASED:
+					keys[e.getKeyCode()] = 0;
+					break;
+				default:
+					break;
+				}
+				return false;
+			}
+		});
+
+	}
+
+	@Override
+	public void paintComponent(final Graphics g) {
+		super.paintComponent(g);
+		final Graphics2D g2d = (Graphics2D) g;
+		g2d.drawImage(texture, 0, 0, null);
+		g2d.drawImage(texture, 0, 512, null);
+		g2d.drawImage(texture, 512, 0, null);
+		g2d.drawImage(texture, 512, 512, null);
+		g2d.drawImage(track, 0, 0, null);
+		raceCar.paintComponent(g2d);
+	}
+
+	private void moveCar() {
+		keyProcessing();
+		if (controlRaceCarPosition()) {
+			raceCar.move();
+		}
+	}
+
+	private boolean controlRaceCarPosition() {
+		/*
+		 * �berpr�fen Sie ob das Fahrzeug den sichtbaren Bereich des Bildschirm
+		 * nicht verl�sst
+		 */
+		final Dimension dim = getSize();
+		if (raceCar.getX() <= 0) {
+			raceCar.move(raceCar.getX() + 1, raceCar.getY());
+			raceCar.stop();
+			return false;
+		}
+		if (raceCar.getX() >= dim.width) {
+			raceCar.move(raceCar.getX() - 1, raceCar.getY());
+			raceCar.stop();
+			return false;
+		}
+		if (raceCar.getY() <= 0) {
+			raceCar.move(raceCar.getX(), raceCar.getY() + 1);
+			raceCar.stop();
+			return false;
+		}
+		if (raceCar.getY() >= dim.height) {
+			raceCar.move(raceCar.getX(), raceCar.getY() - 1);
+			raceCar.stop();
+			return false;
+		}
+
+		/*
+		 * �berpr�fen ob sich das Fahrzueg noch auf der Strecke befindet
+		 */
+		final int carMiddleX = raceCar.getX();
+		final int carMiddleY = raceCar.getY();
+
+		final int trackColor = track.getRGB(carMiddleX, carMiddleY);
+		if (new Color(trackColor).equals(Color.black)) {
+			return true;
+		}
+		if (new Color(trackColor).equals(new Color(0xBDBDBD))) {
+			if (raceCar.getSpeed() >= Car.SLOW_SPEED) {
+				raceCar.slowdown();
+			}
+		} else {
+			if (raceCar.getSpeed() >= Car.SLOW_SPEED) {
+				raceCar.setSlowSpeed();
+			} else if (raceCar.getSpeed() < 0) {
+				raceCar.setMinSlowSpeed();
+			}
+		}
+		return true;
+	}
+
+	private void keyProcessing() {
+
+		if (keys[KeyEvent.VK_UP] == 1) {
+			raceCar.accelerate();
+		}
+		if (keys[KeyEvent.VK_DOWN] == 1) {
+			if (raceCar.getSpeed() > 0) {
+				raceCar.slowdown();
+			} else {
+				raceCar.deccelerate();
+			}
+		}
+		if (keys[KeyEvent.VK_RIGHT] == 1) {
+			raceCar.turnRight();
+		}
+		if (keys[KeyEvent.VK_LEFT] == 1) {
+			raceCar.turnLeft();
+		}
+
+		if (keys[KeyEvent.VK_UP] == 0 && keys[KeyEvent.VK_DOWN] == 0) {
+			if (raceCar.getSpeed() >= 0) {
+				raceCar.slowdown(0.4);
+			} else {
+				raceCar.accelerate();
+			}
+		}
+	}
+
+	@Override
+	public void actionPerformed(final ActionEvent e) {
+		repaint();
+		moveCar();
+	}
+
+	public static void main(final String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+
+	public static void createAndShowGUI() {
+		final JFrame f = new JFrame("Racing");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.add(new Racing());
+		f.setResizable(false);
+		f.pack();
+		f.setVisible(true);
+	}
+
+}
