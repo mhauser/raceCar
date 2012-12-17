94d4ad99de8deac1f65bfac106affd95b8165a02
mhauser <mhauser@hsr.ch>
2012-12-17 21:20:03 +0100

    forgot something =)

---
+++ UI1_Racing/src/newRacing/Car.java
@@ -0,0 +1,113 @@
+package newRacing;
+
+import java.awt.Graphics;
+import java.awt.Point;
+
+import javax.swing.JComponent;
+
+public abstract class Car extends JComponent {
+
+	private static final long serialVersionUID = 4791116509704882431L;
+	public static final double SLOW_SPEED = 1;
+
+	protected float angle = 0;
+	private double speed = 0.0;
+
+	private double x = 0;
+	private double y = 0;
+
+	public Car(final Point start) {
+		x = start.x;
+		y = start.y;
+	}
+
+	@Override
+	protected void paintComponent(final Graphics g) {
+		super.paintComponent(g);
+	}
+
+	public void move() {
+		x = x + Math.sin(angle) * speed;
+		y = y - Math.cos(angle) * speed;
+	}
+
+	public double getXCoordinate() {
+		return x;
+	}
+
+	public double getYCoordinate() {
+		return y;
+	}
+
+	public void accelerate() {
+		if (speed <= getMaxForwardSpeed()) {
+			speed += getAcceleration();
+		}
+	}
+
+	public void deccelerate() {
+		if (speed >= getMaxBackwardSpeed()) {
+			speed -= 0.1;
+		}
+	}
+
+	public void slowdown() {
+		slowdown(1);
+	}
+
+	public void activateBreaks() {
+		slowdown(2);
+	}
+
+	public void slowdown(final double factor) {
+		speed -= 0.2 * factor;
+		if (speed < 0) {
+			speed = 0;
+		}
+	}
+
+	public void slowdownBackward(final double factor) {
+		speed += 0.2 * factor;
+		if (speed > 0) {
+			speed = 0;
+		}
+	}
+
+	public void setSlowSpeed() {
+		if (speed > SLOW_SPEED) {
+			slowdown(2.8);
+		}
+	}
+
+	public void setMinSlowSpeed() {
+		if (speed < -SLOW_SPEED) {
+			speed = -SLOW_SPEED;
+		}
+	}
+
+	public void stop() {
+		speed = 0;
+	}
+
+	public void turnRight() {
+		angle += Math.PI / (40 / getAgility());
+	}
+
+	public void turnLeft() {
+		angle -= Math.PI / (40 / getAgility());
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	// public abstract List<Dimension> getCollisionModel();
+
+	public abstract double getAcceleration();
+
+	public abstract double getAgility();
+
+	public abstract double getMaxForwardSpeed();
+
+	public abstract double getMaxBackwardSpeed();
+}
